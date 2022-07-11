b61a3f6f4916f4f48fd28acd6f1a7cab1b2ab16c
David Gomez Muns <davidmuns@yahoo.es>
2022-07-11 22:32:24 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-class Joke {
-    private joke: string;
-    private score: number;
-    private date: string;
-
-    constructor(joke: string, score: number, date: string) {
-        this.joke = joke;
-        this.score = score;
-        this.date = date;
+window.onload = () => {
+    navigator.geolocation.getCurrentPosition(requestWeatherToApi);
+}
+async function requestWeatherToApi(position: any): Promise<void> {
+    const apiKey = 'd0047952dfbeb9ec30622425fe11ed84';
+    let lon: number = position.coords.longitude;
+    let lat: number = position.coords.latitude;
+    let URL_WEATHER: string = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=es`;
+    let htmlElement: HTMLElement = document.getElementById('weather-in-dom') as HTMLElement;
+    try {
+        let response = await fetch(URL_WEATHER)
+        if (!response.ok) throw new Error(`Error! status: ${response.status}`);
+        let weather = await response.json();
+        console.log(weather);
+        htmlElement.innerHTML = weather.name + " | " + parseInt(weather.main.temp) + "ºC";
+    } catch (e: any) {
+        htmlElement.innerHTML = e.message;
     }
 }
 
@@ -31,6 +39,18 @@ async function requestJokeToApi(): Promise<void> {
     }
 }
 
+class Joke {
+    private joke: string;
+    private score: number;
+    private date: string;
+
+    constructor(joke: string, score: number, date: string) {
+        this.joke = joke;
+        this.score = score;
+        this.date = date;
+    }
+}
+
 const jokeRepository: Joke[] = [];
 function scoreJoke(score: number) {
     requestJokeToApi();
@@ -38,27 +58,3 @@ function scoreJoke(score: number) {
     console.log("Joke repository:", jokeRepository);
 }
 
-function setCoordinatesToApiWeatherUrl() {
-    navigator.geolocation.getCurrentPosition(requestWeatherToApi);
-}
-
-async function requestWeatherToApi(position: any): Promise<void> {
-    const apiKey = 'd0047952dfbeb9ec30622425fe11ed84';
-    let lon: number = position.coords.longitude;
-    let lat: number = position.coords.latitude;
-    let URL_WEATHER: string = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=es`
-    let htmlElement: HTMLElement = document.getElementById('weather-in-dom') as HTMLElement;
-    try {
-        let response = await fetch(URL_WEATHER)
-        if (!response.ok) throw new Error(`Error! status: ${response.status}`);
-        let weather = await response.json();
-        console.log(weather);
-        htmlElement.innerHTML = weather.name + " | " + parseInt(weather.main.temp) + "ºC";
-    } catch (e: any) {
-        htmlElement.innerHTML = e.message;
-    }
-}
-
-window.onload = () => {
-    setCoordinatesToApiWeatherUrl();
-}
